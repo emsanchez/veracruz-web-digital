973e9fd0f36d02e189b6fb13c4979fb4cf6ce341
jmbdeveloper <jmbdeveloper@gmail.com>
2015-06-03 18:49:59 -0500

    Página 404 - Not found - responsive
    Revisión de diseño responsive

--- 404.php
+++ 404.php
@@ -10,7 +10,7 @@ get_header(); ?>
 <link href="<?php bloginfo('template_url'); ?>/css/404.css" rel="stylesheet">
 <div id="main-content" class="not-found">
 	<div class="bloque-izq col-xs-12 col-sm-6 col-md-6">
-		<div class="col-xs-12 col-sm-3 col-md-3"></div>
+		<div class="col-sm-3 col-md-3 hidden-xs"></div>
 		<div class="col-xs-12 col-sm-9 col-md-9">
 			<img class="cuadro-inferior" src="<?php bloginfo('template_url')?>/images/404/cuadro-inferior.png">
 			<div class="lo-sentimos">
@@ -22,14 +22,14 @@ get_header(); ?>
 			<img class="cuadro-superior" src="<?php bloginfo('template_url')?>/images/404/cuadro-superior.png">
 		</div>
 	</div>
-	<div class="bloque-der col-xs-12 col-sm-6 col-md-6">
-		<div class="col-xs-3 col-sm-3 col-md-2"></div>
-		<div class="col-xs-7 col-sm-7 col-md-7">
+	<div class="bloque-der col-sm-6 col-md-6 hidden-xs">
+		<div class="col-md-2 hidden-sm"></div>
+		<div class="col-sm-12 col-md-7">
 		<a href="/" class="veracruz-digital">
 			<img src="<?php bloginfo('template_url')?>/images/404/veracruz-digital.png" alt="Veracruz Digital" >
 		</a>
 		</div>
-		<div class="col-xs-3 col-sm-3 col-md-3"></div>
+		<div class="col-md-3 hidden-sm"></div>
 	</div>
 </div>
 <?php get_footer(); ?>
