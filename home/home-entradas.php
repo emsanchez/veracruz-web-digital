f05938ae1902e537914c36af6ed0ea30d91b32ee
Rolando Drouaillet <rdrouaillet@MacBook-Pro-de-Rolando.local>
2015-06-04 14:36:34 -0500

    Fix hover noticias

--- home/home-entradas.php
+++ home/home-entradas.php
@@ -1,8 +1,19 @@
 <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url')?>/css/component.css" />
 <script src="<?php bloginfo('template_url')?>/js/modernizr.custom.js"></script>
 <?php
-
-	$variablephp = $_GET['filter'];
+	function string2url($cadena) {
+		$cadena = trim($cadena);
+		$cadena = strtr($cadena,
+	"ÀÁÂÃÄÅàáâãäåÒÓÔÕÖØòóôõöøÈÉÊËèéêëÇçÌÍÎÏìíîïÙÚÛÜùúûüÿÑñ",
+	"aaaaaaaaaaaaooooooooooooeeeeeeeecciiiiiiiiuuuuuuuuynn");
+		$cadena = strtr($cadena,"ABCDEFGHIJKLMNOPQRSTUVWXYZ","abcdefghijklmnopqrstuvwxyz");
+		$cadena = preg_replace('#([^.a-z0-9]+)#i', '-', $cadena);
+	        $cadena = preg_replace('#-{2,}#','-',$cadena);
+	        $cadena = preg_replace('#-$#','',$cadena);
+	        $cadena = preg_replace('#^-#','',$cadena);
+		return $cadena;
+	}
+	$variablephp = string2url($_GET['filter']);
 	if($variablephp==""){
 		$variablephp = "noticias";
 	}
