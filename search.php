558b5b02e12dbe01a467a0d9d0e79a15163a13a3
Rolando Drouaillet <rdrouaillet@MacBook-Pro-de-Rolando.local>
2015-06-12 14:35:09 -0500

    Fix search filter

--- search.php
+++ search.php
@@ -1,4 +1,34 @@
 <?php get_header(); ?>
+<?php 
+function custom_search_where($where){
+	global $wpdb;
+	if (is_search())
+		$where .= "OR (t.name LIKE '%".get_search_query()."%' AND {$wpdb->posts}.post_status = 'publish')";
+	return $where;
+}
+ 
+function custom_search_join($join){
+	global $wpdb;
+	if (is_search())
+		$join .= "LEFT JOIN {$wpdb->term_relationships} tr ON {$wpdb->posts}.ID = tr.object_id INNER JOIN {$wpdb->term_taxonomy} tt ON tt.term_taxonomy_id=tr.term_taxonomy_id INNER JOIN {$wpdb->terms} t ON t.term_id = tt.term_id";
+	return $join;
+}
+ 
+function custom_search_groupby($groupby){
+	global $wpdb;
+ 
+	$groupby_id = "{$wpdb->posts}.ID";
+	if(!is_search() || strpos($groupby, $groupby_id) !== false) return $groupby;
+ 
+ 
+	if(!strlen(trim($groupby))) return $groupby_id;
+ 
+		return $groupby.", ".$groupby_id;
+	}
+add_filter('posts_where','custom_search_where');
+add_filter('posts_join', 'custom_search_join');
+add_filter('posts_groupby', 'custom_search_groupby');
+?>
 <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url')?>/css/component.css" />
 <script src="<?php bloginfo('template_url')?>/js/modernizr.custom.js"></script>
 <?php
